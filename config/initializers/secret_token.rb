c9f276902ca7de1480d35c17aa7f8c428edd0aff
Anidhya <anidhyaahuja@gmail.com>
2013-07-04 14:30:18 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mirraw::Application.config.secret_token = 'ded5542363393b601468dbecf7cfb98268c92bf0cee03cd89e4ec3c88694bef5f428f4d0d4552258062aa34ec8a2aa3c45b1e9ad1d380e5564916b83bc7dec32'
